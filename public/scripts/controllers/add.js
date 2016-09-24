f7a4a663eb84aafac168af877c5b87b3a8c41aa6
matt <mattcllhn@gmail.com>
2016-09-24 15:39:55 -0500

    finished functionality

--- public/scripts/controllers/add.js
+++ public/scripts/controllers/add.js
@@ -1,6 +1,21 @@
-myApp.controller("addController", ["$scope", function($scope){
+myApp.controller("addController", ["$scope",'$http', function($scope,$http){
   console.log("hello from add pet controller");
-  $scope.addPet =  function(){
+  $scope.addPet =  function(
+  ){
     console.log('add pet button clicked');
+    var newPet = {
+      name:$scope.name,
+      animal:$scope.animal,
+      age:$scope.age,
+      url:$scope.url
+    };
+    console.log('new pet',newPet);
+    $http({
+    method:'POST',
+     url:'/addPet',
+     data:newPet
+   }).then(function(response){
+   console.log('response from server',response);
+   });//then
   };
 }]);
