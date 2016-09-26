03ebb17b5745cf0738a06cc5cf400b4dac01a384
matt <mattcllhn@gmail.com>
2016-09-26 07:17:41 -0500

    final

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ mongoose.connect(mongoURI);
 
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.http());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.listen(port, function(){
